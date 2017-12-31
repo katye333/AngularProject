38eee73d0a02968590e5fe76d2616926a0f13dac
Katye <katye333@gmail.com>
2017-12-31 05:32:44 -0500

    feat: Unordered list of items added to shopping list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+    // (Upcoming) Model
+    ingredients = [];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    constructor() { }
+    ngOnInit() { }
 
 }

41c6a1eb7f20a26b34e1e7b0592fa221021469e0
Katye <katye333@gmail.com>
2017-12-31 05:37:56 -0500

    feat: Create Ingredient model using typescript shorthand

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: string;
+
+    // constructor(name: string, amount: string) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    // Typescript shortcut for typical class declarations
+    constructor(public name: string, public amount: string) { }
+}

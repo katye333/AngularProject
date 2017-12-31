3499c597e4a5e51614afa77a37e4944a1429c13e
Katye <katye333@gmail.com>
2017-12-31 18:52:00 -0500

    feat: Pass Recipe Data with property binding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+    selector: 'app-recipe-item',
+    templateUrl: './recipe-item.component.html',
+    styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+    // No value assigned to it because recipe is coming from outside data source
+    // Data coming from parent component so we need @Input() decorator
+    @Input() recipe: Recipe;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    constructor() { }
+    ngOnInit() { }
 
 }

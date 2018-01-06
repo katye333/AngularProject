559ca9187e0a8ecc69fea47feaa65c326c16e75b
katye333 <katye333@gmail.com>
2018-01-06 14:06:16 -0500

    feat: Add recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.component.html',
-    styleUrls: ['./recipe-list.component.css']
+    styleUrls: ['./recipe-list.component.css'],
+    providers: [RecipeService]
 })
 export class RecipeListComponent implements OnInit {
-    recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'So good!', 'https://static.pexels.com/photos/257786/pexels-photo-257786.jpeg'),
-        new Recipe('Test Recipe 2', 'Still good', 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=KPTNrvis')
-    ];
 
     // The information contained in recipeWasSelected will be needed in the recipe component
     @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-    constructor() { }
-    ngOnInit() { }
+    recipes: Recipe[]; // set value of recipe array on ngOnInit
+
+    // inject service 
+    // shorthand: assign a property with the same name and angular handles it
+    constructor(private recipeService: RecipeService) { }
+    ngOnInit() {
+        this.recipes = this.recipeService.getRecipes(); // Get a copy of this array from our service
+    }
 
     // The recipes variable defined above is a Recipe object from the recipe.model
     onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,23 @@
-// create the service
+import { Recipe } from './recipe.model';
+
+// the recipes are currently managed in the recipe-list component
+// and should be moved to here to be taken care of
 export class RecipeService {
 
+    // move the array of recipes from recipe-list to here
+    // make it private so we can directly access this array from outside
+    private recipes: Recipe[] = [
+        new Recipe('Test Recipe', 'So good!', 'https://static.pexels.com/photos/257786/pexels-photo-257786.jpeg'),
+        new Recipe('Test Recipe 2', 'Still good', 'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=KPTNrvis')
+    ];
+
+    // access from the outside is not really possible
+    // this method only returns a copy of our array
+    getRecipes() {
+        // will actually return the direct reference to this array
+        // return this.recipes;
+
+        // this returns a new array which is an exact copy of the old one
+        return this.recipes.slice();
+    }
 }

4a42440dfb7b09edcec165b8cb02955f89d589bb
katye333 <katye333@gmail.com>
2018-01-11 08:11:18 -0500

    fix: Remove onNavigate() method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,4 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    // Store the currently selected feature
-    loadedFeature = 'recipe';
-
-    // Update the currently selected feature to the feature received as an argument
-    onNavigate(feature: string) {
-        this.loadedFeature = feature;
-    }
-}
+export class AppComponent { }

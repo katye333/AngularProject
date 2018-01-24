31cb2da09cc775d6600d2c3a37813a3c0720f6d7
katye333 <katye333@gmail.com>
2018-01-24 00:10:57 -0500

    fix: Fixed issue with dropdown directive while upgrading to Bootstrap 4 - Part 2

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,17 +1,21 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
     selector: '[appDropdown]'
 })
 export class DropdownDirective {
+    constructor(private el: ElementRef,
+        private renderer: Renderer2) { }
     // use HostBinding to bind to properties of the element the directive is placed op
     // 'class' is an array of all the classes on that element
-    // this allows us to change the class of the <btn-group> 
-    @HostBinding('class.open') isOpen = false;
+    // this allows us to change the class of the <btn-group>
+    @HostBinding('class.show') isOpen = false;
 
     // listen to a click event
     // execute toggle open method to switch value of this.isOpen
     @HostListener('click') toggleOpen() {
+        let part = this.el.nativeElement.querySelector('.dropdown-menu');
+        this.isOpen ? this.renderer.removeClass(part, 'show') : this.renderer.addClass(part, 'show');
         this.isOpen = !this.isOpen;
     }
 }

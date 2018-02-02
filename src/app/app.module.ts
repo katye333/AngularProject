ceca46295c531c2b7532eb22e332efc86f96daa6
katye333 <katye333@gmail.com>
2018-02-02 17:44:38 -0500

    feat: Use the httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
@@ -8,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -15,7 +15,7 @@ import { CoreModule } from './core/core.module';
     ],
     imports: [
         BrowserModule,
-        HttpModule,
+        HttpClientModule,
         AppRoutingModule,
         SharedModule,
         ShoppingListModule,
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,31 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
 // tslint:disable-next-line:import-blacklist
 import 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { AuthService } from '../auth/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class DataStorageService {
 
-    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) { }
+    constructor(private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
 
     // must return the put request because it is an Observable
     storeRecipes() {
         const token = this.authService.getToken();
 
-        return this.http.put('https://ng-recipe-book-d219c.firebaseio.com/data.json?auth=' + token, this.recipeService.getRecipes());
+        return this.httpClient.put('https://ng-recipe-book-d219c.firebaseio.com/data.json?auth=' + token, this.recipeService.getRecipes());
     }
 
     retrieveRecipes() {
         const token = this.authService.getToken();
 
-        this.http.get('https://ng-recipe-book-d219c.firebaseio.com/data.json?auth=' + token)
+        this.httpClient.get<Recipe[]>('https://ng-recipe-book-d219c.firebaseio.com/data.json?auth=' + token)
             .map(
-            (response: Response) => {
-                const recipes: Recipe[] = response.json();
+            (recipes) => {
                 for (const recipe of recipes) {
                     if (!recipe['ingredients']) {
                         recipe['ingredients'] = [];
